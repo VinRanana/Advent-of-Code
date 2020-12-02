928c0bc964c4aa36cd102577065db0f5c243565f
VinRanana <davinderrana@hotmail.co.uk>
2020-12-02 11:04:52 +0000

    Day 2: Create files and structure

--- 2/main.js
+++ 2/main.js
@@ -1,1 +1,19 @@
-// to do
+const Passwords = require('./passwords.js');
+
+function convertPasswordsToArray (string) {
+  return string.split('\n');
+}
+
+function convertElementsToObjects(array) {
+  return array.forEach(element => {
+
+    // regex to go here
+
+    element = {min, max, character, password}
+  });
+}
+
+let result = convertPasswordsToArray(Passwords.data);
+result = convertElementsToObjects(result);
+
+console.log( result );
