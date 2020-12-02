021465e894ab1cfc6d0a39e28500ab7e2ad5ab4b
VinRanana <davinderrana@hotmail.co.uk>
2020-12-02 15:57:56 +0000

    Day 2: Fixed results of regex

--- 2/main.js
+++ 2/main.js
@@ -4,19 +4,35 @@ function convertPasswordsToArray (string) {
   return string.split('\n');
 }
 
-function convertElementsToObjects(array) {
+function convertElementsToObjects (array) {
   return array.map(element => {
-    // regex to go here
-    const min = element.match(/\d{1,2}/)[0];
-    const max = element.match(/\d{1,2}/g)[1];
-    const character = element.match(/\s([a-z])/)[0];
-    const password = element.match(/\s([a-z]+)/g)[1];
 
-    return {min, max, character, password};
+    const min = Number( element.match(/\d{1,2}/)[0] );
+    const max = Number( element.match(/\d{1,2}/g)[1] );
+    const letter = element.match(/\s([a-z])/)[1];
+    const password = element.match(/\s([a-z]+)$/)[1];
+
+    return {min, max, letter, password};
   });
 }
 
+// function countLetters (array) {
+//   return array.reduce((total, element) => {
+//     let count = 0;
+
+//     for (let i = 0; i < element.password.length; i++) {
+//       if (element.password[i] == element.letter) count++;
+//     }
+
+//     console.log([element.min, element.max, element.letter, element.password, count]);
+//     console.log(total);
+//     if (count >= element.min && count <= element.max) total++;
+//     console.log(total);
+//   }, 0)
+// }
+
 let result = convertPasswordsToArray(Passwords.data);
 result = convertElementsToObjects(result);
+// result = countLetters(result);
 
 console.log( result );
