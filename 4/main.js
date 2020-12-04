9db89d8111afb0a3a1f58e3c43360fdc035ac062
VinRanana <davinderrana@hotmail.co.uk>
2020-12-04 18:41:27 +0000

    Day 4: Convert batch file into array

--- 4/main.js
+++ 4/main.js
@@ -1,2 +1,31 @@
 const BatchFile = require('./batch-file.js');
 
+
+const batchFileArr = makeArrFromStr(BatchFile.data);
+const batchFileObjArr = makeObjArr(batchFileArr);
+
+console.log(batchFileObjArr);
+
+
+function makeArrFromStr (str) {
+  return str.split(/\n\s*\n/);
+}
+
+function makeObjArr (arr) {
+  return arr.map(element => {
+    let obj = {
+      byr: null,
+      iyr: null,
+      eyr: null,
+      hgt: null,
+      hcl: null,
+      ecl: null,
+      pid: null,
+      cid: null,
+    }
+
+    
+
+    return obj;
+  });
+}
