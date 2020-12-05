6b311464f3fb6b277cedb353d29f0f1bf11d24d8
VinRanana <davinderrana@hotmail.co.uk>
2020-12-05 19:22:57 +0000

    Day 5: Find highest binary number

--- 5/main.js
+++ 5/main.js
@@ -1,1 +1,47 @@
 const BoardingPasses = require('./boarding-passes.js');
+
+// Split into array of individual passes.
+const boardingPassesArray = BoardingPasses.data.split('\n')
+
+// Turn each pass into binary numbers
+const binaryArray = boardingPassesToBinary(boardingPassesArray);
+
+// Determine highest binary number
+const binaryNumArray = strArrToNumberArr(binaryArray);
+
+const highestBinary = findHighestBinary(binaryNumArray);
+
+const seatId = 
+
+function boardingPassesToBinary (arr) {
+  return arr.map( element => singlePassToBinary(element) );
+}
+
+function singlePassToBinary (str) {
+  let result = '';
+
+  for (let i = 0; i < 10; i++) {
+    const letter = str[i];
+    if (letter === 'F' || letter === 'L') result += '1';
+    else result += '0';
+  }
+
+  return result;
+}
+
+function strArrToNumberArr (arr) {
+  return arr.map(element => Number(element));
+}
+
+function findHighestBinary (arr) {
+  return arr.reduce((acc, cur) => {
+    if (cur > acc) return cur;
+    else return acc;
+  });
+}
+
+function convertBinaryToNum (binaryNum) {
+  
+}
+
+console.log(highestBinary);
