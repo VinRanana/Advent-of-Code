e01dfa341d3a0f94e084bd220608a12841162990
VinRanana <davinderrana@hotmail.co.uk>
2020-12-07 14:56:16 +0000

    Day 7: Create allBags object

--- 7/main.js
+++ 7/main.js
@@ -1,1 +1,23 @@
 const Rules = require('./rules.js');
+
+// Turn Rules str into arr (split at \n).
+const rulesArray = Rules.data.split('\n');
+// Create bags obj that has each 1st bag as a property.
+const allBags = {};
+
+rulesArray.forEach(element => {
+  const regex = /^[a-z]+\s[a-z]+\sbags/
+
+  allBags[ element.match(regex)[0] ] = [];
+});
+
+console.log(allBags);
+
+// Each 1st bag property has an arr as value.
+// That arr has properties for each bag it can contain.
+
+// Let correctBags = [], finishedBags = [];
+// Each bag that contains shiny gold is pushed to correctBags.
+// Each bag that contains any bag in correctBags is pushed to correctBags,
+// and the bag contained is removed and pushed to finishedBags.
+// Use recursion to complete process and find all bags.
