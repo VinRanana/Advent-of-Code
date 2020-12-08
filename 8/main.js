f02840a2766806d8426376963d5a4d0227383c2c
VinRanana <davinderrana@hotmail.co.uk>
2020-12-08 19:22:05 +0000

    Day 8: Create array of instruction objects

--- 8/main.js
+++ 8/main.js
@@ -1,2 +1,17 @@
 const BootCode = require('./boot-code.js');
+const bootCodeArray = BootCode.data
+  .split('\n')
+  .map(element => {
+    const arr = element.split(' ');
+    const obj = {};
 
+    obj.operation = arr[0];
+    obj.argument = Number(arr[1]);
+
+    return obj
+  })
+
+let accumulator = 0;
+let linesExecuted = [];
+
+console.log(bootCodeArray);
