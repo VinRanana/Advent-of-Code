56684146da1b88943b12e0656bb399471db8e5fc
VinRanana <davinderrana@hotmail.co.uk>
2020-12-09 15:25:36 +0000

    Day 9: Create isNumValid function

--- 9/solution.js
+++ 9/solution.js
@@ -1,2 +1,24 @@
 const Numbers = require('./input.js');
 
+let availableNums = Numbers.data
+  .split('\n')
+  .slice(0, 25)
+  .map( el => Number(el) );
+
+
+console.log(availableNums);
+
+
+
+function isNumValid (num) {
+  let checkedNums = [];
+
+  for (const el of availableNums) {
+    const difference = num - el;
+
+    if (checkedNums.includes(difference) && el != difference) return true;
+    else checkedNums.push(el);
+  }
+
+  return false;
+}
