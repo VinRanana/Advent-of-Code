1b940c0bdb885c64df64af1a2d3174a8b40b26b1
VinRanana <davinderrana@hotmail.co.uk>
2020-12-10 11:53:58 +0000

    Day 10: Pt1 complete. Update README for Pt2

--- 10/solution.js
+++ 10/solution.js
@@ -1,2 +1,43 @@
-const Adapters = require('./input.js');
+const AdaptersString = require('./input.js');
 
+const adaptersArray = AdaptersString.data
+  .split('\n')
+  .map(el => Number(el))
+  .sort((a, b) => a - b);
+
+let joltDifferences = {
+  1: 0,
+  2: 0,
+  3: 1
+};
+
+
+
+joltDifferences = countDifferences(joltDifferences, adaptersArray);
+
+result = multiplyDifferences(joltDifferences, [1, 3]);
+
+console.log(result);
+
+
+
+function countDifferences (differencesObj, adaptersArr) {
+  for (let i = 0, previous = 0; i < adaptersArr.length; i++) {
+    const adapter = adaptersArr[i];
+    const difference = adapter - previous;
+    differencesObj[`${difference}`]++;
+    previous = adapter;
+  }
+
+  return differencesObj
+}
+
+function multiplyDifferences (differencesObj, differencesArr) {
+  let product = 1;
+
+  for (const jolt of differencesArr) {
+    product *= differencesObj[`${jolt}`];
+  }
+
+  return product;
+}
