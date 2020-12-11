7f6656ba9cd6e07b234784bdf21d3e43c2e0a45f
VinRanana <davinderrana@hotmail.co.uk>
2020-12-11 15:09:59 +0000

    Day 11: Create seatLayout 2D array

--- 11/solution.js
+++ 11/solution.js
@@ -1,2 +1,10 @@
+const SeatLayout = require('./input.js');
 const SeatLayoutObj = require('./input.js');
 
+const seatLayout = SeatLayoutObj.data
+  .split('\n')
+  .map( el => el.split('') );
+
+
+  
+console.log(seatLayout);
