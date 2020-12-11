91db71053780aace406c252e4712e677d6cb804c
VinRanana <davinderrana@hotmail.co.uk>
2020-12-11 16:04:34 +0000

    Day 11: Create occupyRow func. Needs debugging.

--- 11/solution.js
+++ 11/solution.js
@@ -1,10 +1,60 @@
-const SeatLayout = require('./input.js');
-const SeatLayoutObj = require('./input.js');
+const SeatLayoutObj = require('./test-input');
 
-const seatLayout = SeatLayoutObj.data
+const emptiedLayout = SeatLayoutObj.data
   .split('\n')
   .map( el => el.split('') );
 
+const occupiedLayout = emptiedLayout.map(el => el);
 
-  
-console.log(seatLayout);
+
+
+occupyRow(emptiedLayout, 1)
+
+console.log(occupiedLayout);
+
+
+
+
+function occupyRow (outerArr, rowIndex) {
+  outerArr[rowIndex].forEach((seat, i, arr) => {
+    if (seat === 'L') {
+      const adjacentSeats = [];
+      let numOfSeatsOccupied;
+
+      checkAdjacentSeats(emptiedLayout, adjacentSeats, rowIndex, i);
+
+      if (numOfSeatsOccupied === 0) occupiedLayout[rowIndex] = '#';
+    }
+  })
+}
+
+function checkAdjacentSeats(seatingLayout, resultArr, rowIndex, i) {
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i - 1));
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 0));
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 1));
+
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 0, i - 1));
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 0, i + 1));
+
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i - 1));
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 0));
+  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 1));
+
+  numOfSeatsOccupied = resultArr.reduce((acc, cur) => {
+    return cur ? acc + 1 : acc;
+  }, 0);
+}
+
+function isSeatOccupied (seatingLayout, rowIndex, seatIndex) {
+  if (seatingLayout[rowIndex][seatIndex] === '#') return true;
+  else return false;
+}
+
+
+
+// console.log(trueFalse('#'));
+
+// function trueFalse (param) {
+//   if (param) return true;
+//   else return false;
+// }
---
+++ 11/test-input.js
@@ -0,0 +1,15 @@
+const SeatLayout = {};
+
+SeatLayout.data =
+`L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`;
+
+module.exports = SeatLayout;
