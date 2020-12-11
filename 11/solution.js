73b291e3ed6229a357af61284a79929da732ae06
VinRanana <davinderrana@hotmail.co.uk>
2020-12-11 20:10:11 +0000

    Day 11: occupyRow func works. Also refactor.

--- 11/solution.js
+++ 11/solution.js
@@ -8,41 +8,41 @@ const occupiedLayout = emptiedLayout.map(el => el);
 
 
 
-occupyRow(emptiedLayout, 1)
+occupyRow(emptiedLayout, 1);
 
 console.log(occupiedLayout);
-
 
 
 
 function occupyRow (outerArr, rowIndex) {
   outerArr[rowIndex].forEach((seat, i, arr) => {
     if (seat === 'L') {
-      const adjacentSeats = [];
-      let numOfSeatsOccupied;
+      const numOfSeatsOccupied = checkAdjacentSeats(emptiedLayout, rowIndex, i);
 
-      checkAdjacentSeats(emptiedLayout, adjacentSeats, rowIndex, i);
-
-      if (numOfSeatsOccupied === 0) occupiedLayout[rowIndex] = '#';
+      if (numOfSeatsOccupied === 0) occupiedLayout[rowIndex][i] = '#';
     }
   })
 }
 
-function checkAdjacentSeats(seatingLayout, resultArr, rowIndex, i) {
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i - 1));
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 0));
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 1));
+function checkAdjacentSeats(seatingLayout, rowIndex, i) {
+  const adjacentSeats = [];
 
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 0, i - 1));
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 0, i + 1));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex - 1, i - 1));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 0));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex - 1, i + 1));
 
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i - 1));
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 0));
-  resultArr.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 1));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex + 0, i - 1));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex + 0, i + 1));
 
-  numOfSeatsOccupied = resultArr.reduce((acc, cur) => {
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex + 1, i - 1));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 0));
+  adjacentSeats.push(isSeatOccupied(seatingLayout, rowIndex + 1, i + 1));
+
+  numOfSeatsOccupied = adjacentSeats.reduce((acc, cur) => {
     return cur ? acc + 1 : acc;
   }, 0);
+
+  return numOfSeatsOccupied;
 }
 
 function isSeatOccupied (seatingLayout, rowIndex, seatIndex) {
