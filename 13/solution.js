da1f59b819e19a925079d104df96ca2b1b3e5b49
VinRanana <davinderrana@hotmail.co.uk>
2020-12-13 13:00:44 +0000

    Day 13: Complete Pt1. Update README for Pt2

--- 13/solution.js
+++ 13/solution.js
@@ -1,2 +1,37 @@
-const BusNotesData = require('./input');
+const BusNotesData = require('./input').data.split('\n');
 
+const timeReadyForBus = BusNotesData[0];
+const busIds = BusNotesData[1]
+  .split(',')
+  .filter(el => el !== 'x')
+  .map(el => Number(el));
+
+
+
+
+const earliestBus = (findEarliestBus(timeReadyForBus, busIds));
+const result = multiplyBusIdByWaitingTime(earliestBus, timeReadyForBus);
+console.log(result);
+
+
+
+
+function findEarliestBus (timeReadyForBus, busIds) {
+  return findEarliestOfEachBus(timeReadyForBus, busIds)
+    .reduce((acc, cur) => {
+      return cur.earliest < acc.earliest ? cur : acc;
+    });
+}
+
+function findEarliestOfEachBus (timeReadyForBus, busIds) {
+  return busIds.map(el => {
+    return {
+      id: el,
+      earliest: Math.ceil(timeReadyForBus / el) * el
+    };
+  });
+}
+
+function multiplyBusIdByWaitingTime ({id, earliest}, timeReadyForBus) {
+  return id * (earliest - timeReadyForBus);
+}
