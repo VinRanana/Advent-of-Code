b588abf074e1050b032b7e620fdf042c314ca949
VinRanana <davinderrana@hotmail.co.uk>
2020-12-15 20:38:03 +0000

    Day 15: Complete Pt1. Update README for Pt2.

--- 15/solution.js
+++ 15/solution.js
@@ -1,2 +1,34 @@
+
 const input = [15, 5, 1, 4, 7, 0];
 
+
+
+
+console.log( playGame(input, 2020) );
+
+
+
+
+function playGame (input, end) {
+  while ( input.length < (end - 1) ) {
+    input.push( nextNumber(input) );
+  }
+
+  return nextNumber(input);
+}
+
+function nextNumber (numArray) {
+  const arrLength = numArray.length;
+  const lastNum = numArray[arrLength - 1];
+  const lastOccurence = findLastOccurence(numArray, arrLength, lastNum);
+
+  return (arrLength - 1) - lastOccurence;
+}
+
+function findLastOccurence (numArray, arrLength, lastNum) {
+  for (let i = arrLength - 2; i >= 0; i--) {
+    if (numArray[i] === lastNum) return i;
+  }
+
+  return arrLength - 1;
+}
