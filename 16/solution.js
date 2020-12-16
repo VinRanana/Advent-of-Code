b407eaa88658dd99cd845c7b039f30d4c0a51400
VinRanana <davinderrana@hotmail.co.uk>
2020-12-16 21:15:26 +0000

    Day 16: Create input arrays

--- 16/solution.js
+++ 16/solution.js
@@ -1,5 +1,29 @@
 
 const Input = require('./input').data.split('\n\n');
 
+const rulesInput = Input[0].split('\n');
+const myTicket = Input[1].split(/[\n,]/).splice(1);
+const nearbyTickets = Input[2].split('\n').splice(1);
+const rulesArr = ruleFactory(rulesInput);
 
-console.log(Input);
+
+
+
+console.log(rulesArr);
+
+
+
+
+function ruleFactory (input) {
+  return input.map(el => {
+    let obj = {}
+
+    obj.ruleName = el.match(/(^.*):/)[1];
+    obj.firstMin = el.match(/\d+/g)[0];
+    obj.firstMax = el.match(/\d+/g)[1];
+    obj.secondMin = el.match(/\d+/g)[2];
+    obj.secondMax = el.match(/\d+/g)[3];
+
+    return obj;
+  })
+}
