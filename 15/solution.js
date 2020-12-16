7bcab7486f6d11a25db06d4a227b787bc6bb343b
VinRanana <davinderrana@hotmail.co.uk>
2020-12-16 02:20:58 +0000

    Day 15: Remake solution. Faster. Still too slow.

--- 15/solution.js
+++ 15/solution.js
@@ -1,38 +1,43 @@
 
-const input = [15,5,1,4,7,0];
+const input = {
+  15: 1, 
+  5: 2,
+  1: 3,
+  4: 4,
+  7: 5 
+};
 
 
 
 
-let result = playGame(input, 3000000);
+let result = playGame(input, 30000000);
 
 console.log( result );
+// 6x faster than first solution, but still 10x too slow
+// Replace obj with map
 
 
 
 
 function playGame (input, end) {
-  const endMinusOne = end - 1;
+  let spoken = 6;
+  let lastNum = 0;
 
-  while ( input.length < endMinusOne ) {
-    input[input.length] = nextNumber(input);
+  if ( !input.hasOwnProperty(`${lastNum}`) ) {
+    input[`${lastNum}`] = spoken;
   }
 
-  return nextNumber(input);
-}
+  while ( spoken < end ) {
+    let newNum = spoken - input[`${lastNum}`];
+    
+    input[`${lastNum}`] = spoken;
+    lastNum = JSON.parse( JSON.stringify(newNum) );
+    spoken++;
 
-function nextNumber (numArray) {
-  const arrLength = numArray.length;
-  const lastNum = numArray[arrLength - 1];
-  const lastOccurence = findLastOccurence(numArray, arrLength, lastNum);
-
-  return (arrLength - 1) - lastOccurence;
-}
-
-function findLastOccurence (numArray, arrLength, lastNum) {
-  for (let i = arrLength - 2; i >= 0; i--) {
-    if (numArray[i] === lastNum) return i;
+    if ( !input.hasOwnProperty(`${lastNum}`) ) {
+      input[`${lastNum}`] = spoken;
+    }
   }
 
-  return arrLength - 1;
+  return lastNum;
 }
